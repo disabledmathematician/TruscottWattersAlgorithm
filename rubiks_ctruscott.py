94e10c5a36f034454e02f2e06041eb184e558724
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2024-12-29 20:55:51 +1100

    Add files via upload

---
+++ rubiks_ctruscott.py
@@ -0,0 +1,444 @@
+"""Initial: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144]
+Consequentual: [[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144]]
+Charles Truscott Watters
+Trying divide and conquer trees
+I love you Tai, I love you Mark
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+B inverse
+Thank you so much edX, MITx and Harvard Online
+Trying to develop a polynomial or logarithmic time Rubik's 2 x 2 algorithm
+[[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144]]
+
+[Program finished]
+""" 
+
+def EncodeToMove(e):
+	if e >> 18 == 1:
+		print("B inverse")
+	elif e >> 17 == 1:
+		print("B2")
+	elif e >> 16 == 1:
+		print("B")
+	elif e >> 15 == 1:
+		print("F inverse")
+	elif e >> 14 == 1:
+		print("F2")
+	elif e >> 13 == 1:
+		print("F")
+	elif e >> 12 == 1:
+		print("D inverse")
+	elif e >> 11 == 1:
+		print("D2")
+	elif e >> 10 == 1:
+		print("D")
+	elif e >> 9 == 1:
+		print("U inverse")
+	elif e >> 8 == 1:
+		print("U2")
+	elif e >> 7 == 1:
+		print("U")
+	elif e >> 6 == 1:
+		print("R inverse")
+	elif e >> 5 == 1:
+		print("R2")
+	elif e >> 4 == 1:
+		print("R")
+	elif e >> 3 == 1:
+		print("L inverse")
+	elif e >> 2 == 1:
+		print("L2")
+	elif e >> 1 == 1:
+		print("L")
+		
+def divconq(n, m, L):
+
+	while m < len(L):
+		print(L[n:m])
+		n += 18
+		m += 18
+		
+def Charles():
+	s = 2 ** 18 + 1
+#	rep = ""
+	rep = []
+	res = []
+#	res = ""
+	n = 0
+	while n <= 18:
+#		print(str(bin(2 **n)[2:]))
+#		print(",")
+#
+		rep.append(2 ** n)
+#		rep += str(bin(2 ** n)[2:])
+#		rep += str(bin(2 ** n))
+#		rep += ','
+#		if n <= 17:
+#			rep += ","
+		n += 1
+	n = 0
+	while n <= 18 ** 2:
+		res.append(rep)
+		n += 18
+#	while n <= 18:
+#		print(str(bin(s)))
+#		rep += str(bin(s))
+#		res += str(bin(s))
+#		s >>= 1
+#		n += 1
+	print("Initial: {}".format(rep))
+	print("Consequentual: {}".format(res))
+#	res = res.split(",")
+#	print(res[0:17])
+#	print(res[18: 2 * 18])
+#	print(res[2 * 18: 3 * 18])
+	print("Charles Truscott Watters")
+	print("Trying divide and conquer trees")
+	print("I love you Tai, I love you Mark")
+	for e in res:
+		for m in e:
+			EncodeToMove(m)
+	print("Thank you so much edX, MITx and Harvard Online")
+	print("Trying to develop a polynomial or logarithmic time Rubik's 2 x 2 algorithm")
+	divconq(0, 18, res)
+	
+Charles()
