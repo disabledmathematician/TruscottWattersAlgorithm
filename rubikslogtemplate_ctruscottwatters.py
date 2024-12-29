2962132fe53e0a268fb263be59421bdf9eb9de5d
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2024-12-29 19:36:43 +1100

    Add files via upload

--- rubikslogtemplate_ctruscottwatters.py
+++ rubikslogtemplate_ctruscottwatters.py
@@ -1,29 +1,394 @@
-"""
-['1', '10', '100', '1000', '10000', '100000', '1000000', '10000000', '100000000', '1000000000', '10000000000', '100000000000', '1000000000000', '10000000000000', '100000000000000', '1000000000000000', '10000000000000000']
-['1', '10', '100', '1000', '10000', '100000', '1000000', '10000000', '100000000', '1000000000', '10000000000', '100000000000', '1000000000000', '10000000000000', '100000000000000', '1000000000000000', '10000000000000000', '100000000000000000']
-['1', '10', '100', '1000', '10000', '100000', '1000000', '10000000', '100000000', '1000000000', '10000000000', '100000000000', '1000000000000', '10000000000000', '100000000000000', '1000000000000000', '10000000000000000', '100000000000000000']
+"""Initial: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072]
+Consequentual: [[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072], [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072]]
 Charles Truscott Watters
 Trying divide and conquer trees
 I love you Tai, I love you Mark
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+L
+L2
+L inverse
+R
+R2
+R inverse
+U
+U2
+U inverse
+D
+D2
+D inverse
+F
+F2
+F inverse
+B
+B2
+Thank you so much edX, MITx and Harvard Online
+Trying to develop a polynomial or logarithmic time Rubik's 2 x 2 algorithm
 
 [Program finished]
 """ 
-
+def EncodeToMove(e):
+	if e >> 18 == 1:
+		print("B inverse")
+	elif e >> 17 == 1:
+		print("B2")
+	elif e >> 16 == 1:
+		print("B")
+	elif e >> 15 == 1:
+		print("F inverse")
+	elif e >> 14 == 1:
+		print("F2")
+	elif e >> 13 == 1:
+		print("F")
+	elif e >> 12 == 1:
+		print("D inverse")
+	elif e >> 11 == 1:
+		print("D2")
+	elif e >> 10 == 1:
+		print("D")
+	elif e >> 9 == 1:
+		print("U inverse")
+	elif e >> 8 == 1:
+		print("U2")
+	elif e >> 7 == 1:
+		print("U")
+	elif e >> 6 == 1:
+		print("R inverse")
+	elif e >> 5 == 1:
+		print("R2")
+	elif e >> 4 == 1:
+		print("R")
+	elif e >> 3 == 1:
+		print("L inverse")
+	elif e >> 2 == 1:
+		print("L2")
+	elif e >> 1 == 1:
+		print("L")
 def Charles():
 	s = 2 ** 18 + 1
-	rep = ""
-	res = ""
+#	rep = ""
+	rep = []
+	res = []
+#	res = ""
 	n = 0
 	while n <= 17:
 #		print(str(bin(2 **n)[2:]))
 #		print(",")
-		rep += str(bin(2 ** n)[2:])
-		if n <= 17:
-			rep += ","
+#
+		rep.append(2 ** n)
+#		rep += str(bin(2 ** n)[2:])
+#		rep += str(bin(2 ** n))
+#		rep += ','
+#		if n <= 17:
+#			rep += ","
 		n += 1
 	n = 0
 	while n <= 18 ** 2:
-		res += rep
+		res.append(rep)
 		n += 18
 #	while n <= 18:
 #		print(str(bin(s)))
@@ -31,14 +396,18 @@ def Charles():
 #		res += str(bin(s))
 #		s >>= 1
 #		n += 1
-#	print("Initial: {}".format(rep))
-#	print("Consequentual: {}".format(res))
-	res = res.split(",")
-	print(res[0:17])
-	print(res[18: 2 * 18])
-	print(res[2 * 18: 3 * 18])
+	print("Initial: {}".format(rep))
+	print("Consequentual: {}".format(res))
+#	res = res.split(",")
+#	print(res[0:17])
+#	print(res[18: 2 * 18])
+#	print(res[2 * 18: 3 * 18])
 	print("Charles Truscott Watters")
 	print("Trying divide and conquer trees")
 	print("I love you Tai, I love you Mark")
-
+	for e in res:
+		for m in e:
+			EncodeToMove(m)
+	print("Thank you so much edX, MITx and Harvard Online")
+	print("Trying to develop a polynomial or logarithmic time Rubik's 2 x 2 algorithm")
 Charles()
