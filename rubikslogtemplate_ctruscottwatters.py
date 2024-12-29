6d1f12c749ca6117b10de55d2e6442b86d7178f6
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2024-12-29 19:06:45 +1100

    Add files via upload
    May get Rubik's cube in logarithmic time. Maybe otherwise depending on the matrix swaps in the RubiksState class

---
+++ rubikslogtemplate_ctruscottwatters.py
@@ -0,0 +1,34 @@
+
+def Charles():
+	s = 2 ** 18 + 1
+	rep = ""
+	res = ""
+	n = 0
+	while n <= 17:
+#		print(str(bin(2 **n)[2:]))
+#		print(",")
+		rep += str(bin(2 ** n)[2:])
+		if n <= 17:
+			rep += ","
+		n += 1
+	n = 0
+	while n <= 18 ** 2:
+		res += rep
+		n += 18
+#	while n <= 18:
+#		print(str(bin(s)))
+#		rep += str(bin(s))
+#		res += str(bin(s))
+#		s >>= 1
+#		n += 1
+#	print("Initial: {}".format(rep))
+#	print("Consequentual: {}".format(res))
+	res = res.split(",")
+	print(res[0:17])
+	print(res[18: 2 * 18])
+	print(res[2 * 18: 3 * 18])
+	print("Charles Truscott Watters")
+	print("Trying divide and conquer trees")
+	print("I love you Tai, I love you Mark")
+
+Charles()
